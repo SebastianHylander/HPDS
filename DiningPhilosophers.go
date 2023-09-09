8701a6459a546cffa2c5a138e328382fbcf7bb6c
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-09-09 13:23:11 +0200

    Plan for the problem solving

---
+++ DiningPhilosophers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//Udlever forks til philo (tilfældigt) - concurrency
+	//Hvis man har 1 fork, print "thinking", og læg den ned igen.
+	//Udlever hele tiden indtil en får 2 forks.
+	//Hvis man har 2 forks, print "eating", og læg begge ned igen.
+	//Hvis man har 1 fork print "thinking"
+
+	fmt.Println("")
+}
