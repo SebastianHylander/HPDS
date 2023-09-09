5a4fadfd3319a7c8df20d11420b4df41c5114179
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-09-09 23:46:04 +0200

    Bare udkast

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -2,15 +2,43 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
+const nPhil = 5
 
-	//Udlever forks til philo (tilfældigt) - concurrency
+func main() {
+	//Udlever forks til philo (async)
 	//Hvis man har 1 fork, print "thinking", og læg den ned igen.
 	//Udlever hele tiden indtil en får 2 forks.
 	//Hvis man har 2 forks, print "eating", og læg begge ned igen.
-	//Hvis man har 1 fork print "thinking"
 
-	fmt.Println("")
+	leftFork := make(chan bool)
+	rightFork := make(chan bool)
+
+	for i := 1; i <= nPhil; i++ {
+		go Philosophers(i, leftFork, rightFork)
+		//recieve message:
+		/* msg := <-leftFork
+		msg1 := <-rightFork
+		fmt.Println(i, msg, msg1) */
+	}
+}
+
+func Philosophers(id int, leftFork chan bool, rightFork chan bool) {
+	for i := 0; i < 3; i++ { //All should eat at least 3 times
+		fmt.Printf("Philosopher %d is thinking\n", id)
+		time.Sleep(time.Millisecond * 500)
+
+		//request forks, send message
+		fmt.Printf("Philosopher %d request forks\n", id)
+		<-leftFork
+		<-rightFork
+
+		//release forks
+		//OBS: Der skal være en betingelse, at når en af dem er true, og den anden er false, så realeaser man (deadlock)
+		fmt.Printf("Philosopher %d has eaten; releasing forks\n", id)
+		leftFork <- true
+		rightFork <- true
+	}
 }
