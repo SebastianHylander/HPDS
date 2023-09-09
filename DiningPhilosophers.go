ade17a515a04734a2e2dd192d9ad7348269f0cef
edward.rostomian@gmail.com <edward.rostomian@gmail.com>
2023-09-10 00:11:00 +0200

    WaitGroup (for completing goroutines)

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,12 +13,20 @@ func main() {
 	//Hvis man har 1 fork, print "thinking", og læg den ned igen.
 	//Udlever hele tiden indtil en får 2 forks.
 	//Hvis man har 2 forks, print "eating", og læg begge ned igen.
+	var wg sync.WaitGroup
+	wg.Add(1) //inkrementere med 1, der er 1 goroutine at vente på
 
 	leftFork := make(chan bool)
 	rightFork := make(chan bool)
 
 	for i := 1; i <= nPhil; i++ {
-		go Philosophers(i, leftFork, rightFork)
+		go func() {
+			Philosophers(i, leftFork, rightFork)
+			wg.Done() //dekrementere antal goroutines
+		}()
+
+		wg.Wait() //Blokere, venter på alle goroutines er færdige
+
 		//recieve message:
 		/* msg := <-leftFork
 		msg1 := <-rightFork
