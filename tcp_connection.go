9b34a9a21878556c8c8e513fc0a1155e2f0ece43
Sebastian Cloos Hylander <sehy@itu.dk>
2023-09-20 11:26:12 +0200

    sdlfhsdhf

---
+++ tcp_connection.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	input := true
+	for input {
+		var line string
+		fmt.Println("Do you want to host? (Write 'host')")
+		fmt.Println("Do you want to connect? (Write 'connect')")
+		fmt.Println("Do you want to exit? (Write 'exit')")
+		fmt.Scanln(&line)
+
+		if line == "host" {
+			host()
+			input = false
+		} else if line == "connect" {
+			getIp()
+			input = false
+		} else if line == "exit" {
+			input = false
+		} else {
+			fmt.Println("Invalid input!")
+		}
+	}
+
+}
+
+func getIp() {
+	var ip string
+	fmt.Println("Write desired ip address (if empty the ip is 'localhost'):")
+	fmt.Scanln(&ip)
+	if ip == "" {
+		ip = "localhost"
+	}
+	connect(ip)
+}
+
+func connect(ip string) {
+	conn, err := net.Dial("tcp", ip+":8080")
+	if err != nil {
+		// handle error
+	}
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+
+}
+
+func host() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			//handle error
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	fmt.Println("I GOT A REQUEST!!!!!!")
+}
