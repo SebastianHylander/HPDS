2ea4f2071121f4477e9873c9a1b757bae833d321
Sebastian Cloos Hylander <sehy@itu.dk>
2023-11-15 11:50:45 +0100

    Node should be done

---
+++ Auction/Node/node.go
@@ -0,0 +1,184 @@
+package Node
+
+import (
+	"context"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	proto "github.com/SebastianHylander/HPDS/Auction/grpc"
+	"google.golang.org/grpc"
+)
+
+type Node struct {
+	proto.UnimplementedAuctionSystemServer
+	ip           string
+	port         int
+	id           int64
+	leaderId     int64
+	nodeId2Index map[int]int
+	neighbour    int
+	replications []proto.AuctionSystemClient
+	bids         map[int64]int64
+	inProgress   bool
+}
+
+func NewNode(ip string, port int) *Node {
+	node := &Node{
+		ip:   ip,
+		port: port,
+	}
+	return node
+}
+
+func (n *Node) Start(nodes []string) {
+	go n.startServer()
+
+	for _, node := range nodes {
+		// Split the string into ip and port
+		ipAndPort := strings.Split(node, ":")
+		port, _ := strconv.Atoi(ipAndPort[1])
+		if !(port == n.port) {
+			// make grpc connection to each node
+			connection, err := n.connectToNeighbour(ipAndPort[0], port)
+			if err != nil {
+				log.Fatalf("Could not connect to node: %v", err)
+			}
+			n.replications = append(n.replications, connection)
+		} else {
+			n.neighbour = len(n.replications) - 1
+			if n.neighbour < 0 {
+				n.neighbour = len(nodes) - 1
+			}
+			n.replications = append(n.replications, nil)
+		}
+		// append id to the list of ids
+		n.nodeId2Index[port] = len(n.nodeId2Index)
+
+	}
+	n.inProgress = true
+	if n.replications[0] == nil {
+		n.RunElection(context.Background(), &proto.ElectionStatus{ServerId: n.id})
+	}
+	go n.run()
+
+}
+
+func (n *Node) run() {
+	time.Sleep(20 * time.Second)
+	n.inProgress = false
+	for {
+	}
+
+}
+
+func (n *Node) RunElection(ctx context.Context, in *proto.ElectionStatus) (*proto.Empty, error) {
+	if in.ServerId == n.id {
+		log.Print("Election done")
+		return &proto.Empty{}, nil
+	} else if in.ServerId < n.id {
+		in.ServerId = n.id
+	}
+	n.leaderId = in.ServerId
+
+	runElection := true
+	for runElection {
+		_, err := n.replications[n.neighbour].RunElection(ctx, in)
+		if err != nil {
+			n.neighbour = (n.neighbour - 1)
+			if n.neighbour < 0 {
+				n.neighbour = len(n.replications) - 1
+			}
+			if n.replications[n.neighbour] == nil {
+				n.leaderId = n.id
+				runElection = false
+			}
+		} else {
+			runElection = false
+		}
+
+	}
+
+	return &proto.Empty{}, nil
+}
+
+func (n *Node) MakeBid(ctx context.Context, in *proto.Bid) (*proto.Ack, error) {
+	if n.inProgress {
+		if n.leaderId == n.id {
+			n.updateBid(in.Id, in.Amount)
+			in.FromLeader = true
+			for _, node := range n.replications {
+				if node != nil {
+					node.MakeBid(ctx, in)
+				}
+			}
+		} else {
+			if in.FromLeader {
+				n.updateBid(in.Id, in.Amount)
+			} else {
+				_, err := n.replications[n.nodeId2Index[int(n.leaderId)]].MakeBid(ctx, in)
+				if err != nil {
+					n.RunElection(ctx, &proto.ElectionStatus{ServerId: n.id})
+					n.MakeBid(ctx, in)
+				}
+			}
+		}
+	}
+	return &proto.Ack{}, nil
+}
+
+func (n *Node) updateBid(bidderId int64, bidValue int64) {
+	n.bids[bidderId] = bidValue
+}
+
+func (n *Node) GetResult(ctx context.Context, in *proto.Empty) (*proto.Result, error) {
+	if n.leaderId == n.id {
+		if len(n.bids) == 0 {
+			return &proto.Result{Result: "No bids"}, nil
+		}
+		highestBidder := int64(0)
+		for bidder, bid := range n.bids {
+			if bid > n.bids[highestBidder] {
+				highestBidder = bidder
+			}
+		}
+		var result string
+		if n.inProgress {
+
+			result = "Auction still in progress: Current winner is bidder " + strconv.Itoa(int(highestBidder)) + " with a bid of " + strconv.Itoa(int(n.bids[highestBidder]))
+		} else {
+			result = "Auction finished: Winner is bidder " + strconv.Itoa(int(highestBidder)) + " with a bid of " + strconv.Itoa(int(n.bids[highestBidder]))
+		}
+		return &proto.Result{Result: result}, nil
+	} else {
+		return n.replications[n.nodeId2Index[int(n.leaderId)]].GetResult(ctx, in)
+	}
+}
+
+func (n *Node) startServer() {
+	// Create a new grpc server
+	grpcServer := grpc.NewServer()
+
+	// Make the server listen at the given port (convert int port to string)
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(n.port))
+	if err != nil {
+		log.Fatalf("Could not create the server %v", err)
+	}
+	log.Printf("Started server at port: %d\n", n.port)
+
+	// Register the grpc server and serve its listener
+	proto.RegisterAuctionSystemServer(grpcServer, n)
+	serveError := grpcServer.Serve(listener)
+	if serveError != nil {
+		log.Fatalf("Could not serve listener")
+	}
+}
+
+func (n *Node) connectToNeighbour(ip string, port int) (proto.AuctionSystemClient, error) {
+	// Dial the server at the specified port.
+	conn, err := grpc.Dial(ip+":"+strconv.Itoa(port), grpc.WithInsecure())
+
+	return proto.NewAuctionSystemClient(conn), err
+}
---
+++ Auction/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/SebastianHylander/HPDS/Mutual_Exclusion/Node"
+)
+
+type NodeStruct struct {
+	ip         string
+	portNumber int
+}
+
+func main() {
+	// Read the file
+	file, err := os.Open("nodes.txt")
+	if err != nil {
+		log.Fatalf("Could not open file: %v", err)
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file
+	scanner := bufio.NewScanner(file)
+
+	// Create a slice to store the nodes
+	var nodes []NodeStruct
+
+	// Read the file line by line
+	for scanner.Scan() {
+		// Get the line
+		line := scanner.Text()
+
+		// Split the line into a slice of strings
+		split := strings.Split(line, " ")
+
+		// Convert the strings to the correct types
+		ip := split[0]
+
+		port, err := strconv.Atoi(split[1])
+		if err != nil {
+			log.Fatalf("Could not convert port to int: %v", err)
+		}
+
+		// Create a node
+		node := NodeStruct{
+			ip:         ip,
+			portNumber: port,
+		}
+
+		// Add the node to the slice
+		nodes = append(nodes, node)
+	}
+
+	hasToken := true
+
+	// Print the nodes
+	for i := range nodes {
+		// make a Node from node.go with the given ip and port
+		// call the function to start the node
+
+		newNode := Node.NewNode(nodes[i].ip, nodes[i].portNumber)
+
+		go newNode.Start(nodes[(i+1)%len(nodes)].ip, nodes[(i+1)%len(nodes)].portNumber, hasToken)
+		hasToken = false
+	}
+	for {
+	}
+}
